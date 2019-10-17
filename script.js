e55ccb11f174f06fb791abfec431d918fd4b2d42
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-18 00:39:25 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let container = document.getElementById('thumbs');
-container.addEventListener('click', event => {
-  if (!event.target.closest('a')) {
-    return;
-  }
-  event.preventDefault();
-  let largeImage = document.getElementById('largeImg');
-  largeImage.src = event.target.closest('a').href;
-});
