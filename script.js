8ac60a35a996648f169e99b2ba580cade653b39f
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-21 13:57:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+'use strict';
+
 const container = document.getElementById('thumbs');
-const largeImage = document.getElementById('largeImg');
+const largeImage = document.getElementById('largeImg'); 
 
 container.addEventListener('click', event => {
   if (!event.target.closest('a')) {
     return;
   }
-  
+
   event.preventDefault();
-  
+
   largeImage.src = event.target.closest('a').href;
 });
