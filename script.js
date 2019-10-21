40ee330b6fbe57eaa0a4478a8cbc794821b85ba4
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-10-21 13:51:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let container = document.getElementById('thumbs');
+const container = document.getElementById('thumbs');
+const largeImage = document.getElementById('largeImg');
+
 container.addEventListener('click', event => {
   if (!event.target.closest('a')) {
     return;
   }
+  
   event.preventDefault();
-  let largeImage = document.getElementById('largeImg');
+  
   largeImage.src = event.target.closest('a').href;
 });
